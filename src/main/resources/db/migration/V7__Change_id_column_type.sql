9ae90b5bcf95769c18675127afcffa2b60e2a2d1
small_chen <3300754579@qq.com>
2020-01-16 20:59:00 +0800

    修复sql版本7的语法错误

--- src/main/resources/db/migration/V7__Change_id_column_type.sql
+++ src/main/resources/db/migration/V7__Change_id_column_type.sql
@@ -1,4 +1,4 @@
 /*alter table question alter column id bigint auto_increment;
 alter table user alter column id bigint auto_increment;*/
-alert table question modify id bigint auto_increment;
-alert table 'user' modify id bigint auto_increment;
+alter table question modify id bigint auto_increment;
+alter table 'user' modify id bigint auto_increment;
