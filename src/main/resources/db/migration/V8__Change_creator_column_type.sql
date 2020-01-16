738854d7aca2d99b160e4ee27012e09979a69467
small_chen <3300754579@qq.com>
2020-01-16 22:55:07 +0800

    修复版本8数据库文件的错误

--- src/main/resources/db/migration/V8__Change_creator_column_type.sql
+++ src/main/resources/db/migration/V8__Change_creator_column_type.sql
@@ -1,4 +1,4 @@
 /*alter table question alter column creator bigint;
 alter table comment alter column commentator bigint;*/
-alter table question modify creator bigint
-alter table comment modify commentator bigint
+alter table question modify creator bigint;
+alter table comment modify commentator bigint;
